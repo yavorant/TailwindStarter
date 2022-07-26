4a3beb15aef120d441e17a7e576a7babb0a6d5d7
yavorant <yavorant@gmail.com>
2022-07-27 00:09:40 +0300

    first one

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], //watch all html files in current folder
+  // content: ['.src/**/*.{html,js}'], //watch all html and js in src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
